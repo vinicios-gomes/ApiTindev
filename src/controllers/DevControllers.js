88c8ad8caa2ca6a3f8d5eb2bb1cc0fd349fb03b8
Vinicios Gomes <viniciusgomesviana@gmail.com>
2019-08-07 00:18:14 -0300

    Verificacao se user ja existe no bd

--- src/controllers/DevControllers.js
+++ src/controllers/DevControllers.js
@@ -5,6 +5,11 @@ module.exports = {
   async store(req, res) {
     const { username } = req.body;
 
+    const userExist = await Dev.findOne({ user: username });
+    if (userExist) {
+      return res.json(userExist);
+    }
+
     const response = await axios.get(
       `https://api.github.com/users/${username}`
     );
